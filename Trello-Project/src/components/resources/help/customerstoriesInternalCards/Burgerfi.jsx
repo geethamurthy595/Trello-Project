1a7a638588c22cfdfc7edfbaada5797561a153a7
Pranav chandraa <pranavchandraa07@gmail.com>
2026-02-20 09:56:18 +0530

    customer folder is added

---
+++ Trello-Project/src/components/resources/help/customerstoriesInternalCards/Burgerfi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Burgerfi = () => {
+  return (
+    <div>Burgerfi</div>
+  )
+}
+
+export default Burgerfi
---
+++ Trello-Project/src/components/resources/help/customerstoriesInternalCards/Unicef.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Unicef = () => {
+  return (
+    <div>Unicef</div>
+  )
+}
+
+export default Unicef
