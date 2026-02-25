87ddc5aea1bb6101f2d9f11dfc5665d1344fbc72
sameer <sameer@gmail.com>
2026-02-25 09:41:25 +0530

    implemented DoSomething page

--- Trello-Project/src/router/Router.jsx
+++ Trello-Project/src/router/Router.jsx
@@ -169,6 +169,8 @@ import AutomationPopup from "../components/boards/taskbar/taskbarAutomationPopup
 import AutomationRules from "../components/boards/taskbar/taskbarAutomationPopup/AutomationRules";
 import ActivityPopup from "../components/boards/taskbar/taskbarAutomationPopup/ActivityPopup";
 import AutomationDuedate from "../components/boards/taskbar/taskbarAutomationPopup/AutomationDuedate";
+import DoSomething from "../components/resources/help/customerstoriesInternalCards/DoSomething";
+
 
 
 let routes = createBrowserRouter([
@@ -917,6 +919,10 @@ let routes = createBrowserRouter([
         path: "/buttomNavbar",
         element: <ButtomNavbar/>,
       },
+      {
+        path:"/components/resources/help/customerstoriesInternalCards/dosomething",
+        element:<DoSomething/>
+      }
 ]);
 
 export default routes;
