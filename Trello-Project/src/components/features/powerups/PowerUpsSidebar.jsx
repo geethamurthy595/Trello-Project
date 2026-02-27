d7c8fec130860bac202075426c7a0e9a9ea2e5c9
Ajay Sagar <sagarajay913@gmail.com>
2026-02-27 13:53:23 +0530

    Sales and Support task is completed

--- Trello-Project/src/components/features/powerups/PowerUpsSidebar.jsx
+++ Trello-Project/src/components/features/powerups/PowerUpsSidebar.jsx
@@ -108,9 +108,11 @@ const PowerUpsSidebar = () => {
       <div className="flex items-center gap-1 py-1 pl-8 hover:bg-[#2B2C2F] rounded mb-2 cursor-pointer">
         <p className="w-full font-semibold  m-0 p-0">Product & design</p>
       </div>
-      <div className="flex items-center gap-1 py-1 pl-8 hover:bg-[#2B2C2F] rounded mb-2 cursor-pointer">
+      <Link to='category/sales-support'>
+        <div className="flex items-center gap-1 py-1 pl-8 hover:bg-[#2B2C2F] rounded mb-2 cursor-pointer">
         <p className=" w-full font-semibold  m-0 p-0">Sales & support</p>
       </div>
+      </Link>
       <p className="text-xs py-1 pl-8">
         Looking for browser extensions, add-ons, and more?
         <span className="text-[#669DF1] underline cursor-pointer">
